043d26f3e1d587d4a9ff5033ff110ae37c3db63a
Peter Kolloch <kolloch@web-app-evolution.com>
2014-05-03 22:40:42 +0200

    Import start/stop behavior

--- docker-cassandra-opscenter/init.sh
+++ docker-cassandra-opscenter/init.sh
@@ -1,10 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
 set -e
 
 . config.sh
 
 echo "Starting OpsCenter on $IP..."
-service opscenterd start
-service ssh start
-tail -f /var/log/opscenter/opscenterd.log
+exec /usr/share/opscenter/bin/opscenter -f
